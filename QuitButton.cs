0e282a752db64f532bdb4b7ee5fd10c40bc4437d
joebker1 <72422912+joebker1@users.noreply.github.com>
2024-07-24 20:21:29 -0700

    Update QuitButton.cs

--- QuitButton.cs
+++ QuitButton.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+/*
+Description: This is the script to quit the game attached to a button in the pause menu.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
