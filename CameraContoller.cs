518923d3a0ce79cc5af682f30f3daabb749bbfe7
joebker1 <72422912+joebker1@users.noreply.github.com>
2024-07-24 20:11:35 -0700

    Add files via upload

---
+++ CameraContoller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CameraContoller : MonoBehaviour
+{
+	public GameObject player;
+	
+	
+	private Vector3 offset;
+	
+	
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+	
+}
